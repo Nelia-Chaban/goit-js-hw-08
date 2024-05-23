31a6350d3046a4f217c0a66b300f5dd0c7d102cd
Nelia-Chaban <126400490+Nelia-Chaban@users.noreply.github.com>
2024-05-23 18:54:55 +0300

    first commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,6 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
