dc6154fdf4a8234fc5d3d1447614e77a17cecfaf
Nelia-Chaban <126400490+Nelia-Chaban@users.noreply.github.com>
2024-05-27 21:26:00 +0300

    feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+import { common } from './common';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+let formData = JSON.parse(localStorage.getItem(common.KEY_EMAIL_MESSAGE)) ?? {};
+
+const { email, message } = form.elements;
+pageReload();
+
+function onInput() {
+  formData = { email: email.value, message: message.value };
+  localStorage.setItem(common.KEY_EMAIL_MESSAGE, JSON.stringify(formData));
+}
+
+function pageReload() {
+  if (formData) {
+    email.value = formData.email ?? '';
+    message.value = formData.message ?? '';
+  }
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  if (email.value === '' || message.value === '') {
+    alert('Please fill in all fields!');
+  }
+  console.log({ email: email.value, message: message.value });
+  localStorage.removeItem(common.KEY_EMAIL_MESSAGE);
+  evt.currentTarget.reset();
+}
--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,6 @@
 const common = {
   KEY_CURRENT_TIME: 'videoplayer-current-time',
+  KEY_EMAIL_MESSAGE: 'feedback-form-state',
 };
 
 export { common };
